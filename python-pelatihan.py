1690eb4cf3d1455bff74ae43b0e2a543920c07cd
ryannchairullmaqqi <111479862+ryannchairullmaqqi@users.noreply.github.com>
2022-08-22 12:17:39 +0700

    Add files via upload

---
+++ python-pelatihan.py
@@ -0,0 +1,19 @@
+print("halo semuanya")
+nama_saya_adalah = input("\nnama saya adalah")
+print("\nhari ini kita akan belajar pemrograman")
+print("kita akan belajar mengkonversi celcius ke fahrenheit")
+print("kita mulai bahas rumus nya dulu yaaa \nrumus celcius ke fahrenheit adalah: (9/5 * celcius + 32) \noke setelah kita mengerti rumus nya mari kita coba")
+
+x = int(input("\ncontoh suhu celcius"))
+y = (9/5 * x) + 32
+print(x,"celcius =",y,"fahrenheit")
+
+print("\noke setelah kita juga bisa melakukan perhitungan di python dengan bilangan bulat dan bilangan tidak bulat")
+
+A = int(input("\nmasukan nilai bilangan bulat: "))
+B = float(input("\nmasukan nilai bilangan tidak bulat :"))
+C = A + B
+
+print("A -", A)
+print("B -", B)
+print("C -", C)
