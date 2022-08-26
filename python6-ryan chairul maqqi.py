3ce2a32f3cce8f5ce58f9fa37fa86f2769fea8c6
ryannchairullmaqqi <111479862+ryannchairullmaqqi@users.noreply.github.com>
2022-08-26 08:58:57 +0700

    Add files via upload

---
+++ python6-ryan chairul maqqi.py
@@ -0,0 +1,21 @@
+alas = int (input("masukan nilai alas"))
+tinggi = int (input("masukan nilai tinggi"))
+luas_s = alas * tinggi /2
+
+panjang = int (input ("masukan panjang:"))
+lebar = int (input("masukan lebar:"))
+keliling = 2* (panjang + lebar)
+luas = panjang * lebar
+print ("keliling=",keliling)
+print ("luas:",luas)
+
+r = int (input("masukan jari jari:"))
+keliling = 2 * 3.14 * r
+luas = 3.14 * r * r
+print ("kkeliling =",keliling)
+print ("luas =",luas)
+
+alas = float(input("nilai alas:"))
+tinggi = float(input("nilai tinggi:"))
+luas_jg = alas * tinggi
+print("luas jajar genjang adalah",luas_jg)
