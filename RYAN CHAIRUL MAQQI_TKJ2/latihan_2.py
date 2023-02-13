48e853462706efe70687b48f3cd52412533f7ef5
ryannchairullmaqqi <111479862+ryannchairullmaqqi@users.noreply.github.com>
2023-02-13 09:57:02 +0700

    Add files via upload

---
+++ RYAN CHAIRUL MAQQI_TKJ2/latihan_2.py
@@ -0,0 +1,16 @@
+'''
+Buatlah program menghitung luas bangun datar menggunakan fungsi. Gunakan parameter
+-segitiga
+-persegi panjang
+'''
+def luas_s(alas, tinggi):
+    luas = 1/2 * alas * tinggi
+    print("luas segitiga adalah", luas)
+
+luas_s(5, 1)
+
+def luas_pp(panjang, lebar):
+    luas = panjang + lebar
+    print("luas persegi panjang", luas)
+
+luas_pp(7, 3)
---
+++ RYAN CHAIRUL MAQQI_TKJ2/latihan_fungsi.py
@@ -0,0 +1,44 @@
+'''Fungsi pada python didefinisikan dengan def'''
+
+'''def nama_fungsi():'''
+
+def salam():
+    print("Assalamu'alaikum !! Akhi Ukhty")
+
+salam()
+salam()
+salam()
+
+'''Buatlah sebuah fungsi versi kalian'''
+
+def jawab():
+    print("wa'alaikumsalam")
+
+salam()
+salam()
+salam()
+
+'''Fungsi dengan parameter'''
+'''def nama_fungsi(p1, p2)'''
+
+def say_hi(nama):
+   print("Halo perkenalkan nama saya", nama)
+
+say_hi("jawir")
+say_hi("reog")
+
+'''Buatlah fungsi dengan 3 Parameter'''
+
+def nama_hewan(hewan1, hewan2, hewan3):
+    print("hewan adalah:", hewan1)
+    print("hewan adalah:", hewan2)
+    print("hewan adalah:", hewan3)
+
+nama_hewan(hewan1 = "ular", hewan2 = "jerapah", hewan3 = "semut")
+
+def perkenalan(nama, alamat, umur):
+    print("nama saya:", nama)
+    print("alamat saya:", alamat)
+    print("umur saya:", umur)
+perkenalan("ryan", "sugutamu", "16")
+print("\n\n======= BATAS =======\n\n")
